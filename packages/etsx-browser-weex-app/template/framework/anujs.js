70469c949656bf1274ffa9f1da81d99106ae1a08
余重桦 <yuchonghua@163.com>
2019-04-08 21:03:51 +0800

    module.id

--- packages/etsx-browser-weex-app/template/framework/anujs.js
+++ packages/etsx-browser-weex-app/template/framework/anujs.js
@@ -1,10 +1,9 @@
 import Framework from './base'
 import { getDefault } from '../config'
-const modulesId = module.id
 export default class AnujsFramework extends Framework {
   static getFramework (context) {
     if (context && context.loadedAsync) {
-      context.loadedAsync.add(modulesId)
+      context.loadedAsync.add(module.id)
     }
     return import('anujs').then((f) => new AnujsFramework(getDefault(f)))
   }
--- packages/etsx-browser-weex-app/template/framework/raxjs.js
+++ packages/etsx-browser-weex-app/template/framework/raxjs.js
@@ -1,10 +1,9 @@
 import Framework from './base'
 import { getDefault } from '../config'
-const modulesId = module.id
 export default class RaxjsFramework extends Framework {
   static getFramework (context) {
     if (context && context.loadedAsync) {
-      context.loadedAsync.add(modulesId)
+      context.loadedAsync.add(module.id)
     }
     return Promise.all([import('rax'), import('driver-dom')]).then(([f, driver]) => new RaxjsFramework(getDefault(f), getDefault(driver)))
   }
